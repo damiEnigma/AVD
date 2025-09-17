a199b51eb589feb419139f57d68c47daeb8752a9
damiEnigma <dpackard@fmglaw.com>
2025-09-17 11:10:57 -0500

    Update OneDrive.ps1

--- OneDrive.ps1
+++ OneDrive.ps1
@@ -1,1 +1,3 @@
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive' -Name 'SilentAccountConfig' -Value 1
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive' -Name 'FilesOnDemandEnabled' -Value 1
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive' -Name 'GPOSetUpdateRing' -Value 5
